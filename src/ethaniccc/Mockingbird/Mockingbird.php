c0029025c029347f23583575f020c3d45eadc260
ethaniccc <schooldocumentsandstuff@gmail.com>
2020-07-01 13:09:03 -0700

    remove aimbot checks

--- src/ethaniccc/Mockingbird/Mockingbird.php
+++ src/ethaniccc/Mockingbird/Mockingbird.php
@@ -37,7 +37,7 @@ class Mockingbird extends PluginBase implements Listener{
     private $database;
     private $modules = [
         "Combat" => [
-            "Reach", "Aimbot", "AutoClickerA", "AutoClickerB", "AutoClickerC",
+            "Reach", "AutoClickerA", "AutoClickerB", "AutoClickerC",
             "AutoClickerD", "ToolboxKillaura", "NoKnockback"
         ],
         "Movement" => [
