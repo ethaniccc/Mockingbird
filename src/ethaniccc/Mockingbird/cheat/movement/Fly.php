3f50a4d3344df7cf3598c4ec47502b4fd092a009
ethaniccc <schooldocumentsandstuff@gmail.com>
2020-07-05 16:52:31 -0700

    Attempt to fix false positive when player is hit

--- src/ethaniccc/Mockingbird/cheat/movement/Fly.php
+++ src/ethaniccc/Mockingbird/cheat/movement/Fly.php
@@ -8,10 +8,13 @@ use ethaniccc\Mockingbird\utils\LevelUtils;
 use pocketmine\block\Air;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\Player;
 
 class Fly extends Cheat{
 
     private $ticksOffGround = [];
+    private $wasDamaged = [];
 
     public function __construct(Mockingbird $plugin, string $cheatName, string $cheatType, bool $enabled = true){
         parent::__construct($plugin, $cheatName, $cheatType, $enabled);
@@ -40,12 +43,12 @@ class Fly extends Cheat{
                 $continue = false;
             }
         }
-        if($continue && !$player->isOnGround()){
+        if($continue && !$player->isOnGround() && !$this->wasPreviouslyHit($name)){
             if(!isset($this->ticksOffGround[$name])){
                 $this->ticksOffGround[$name] = 0;
             }
             $this->ticksOffGround[$name] += 1;
-            if($this->ticksOffGround[$name] > 100){
+            if($this->ticksOffGround[$name] > 60){
                 $this->addViolation($name);
                 $this->notifyStaff($name, $this->getName(), $this->genericAlertData($player));
             }
@@ -56,8 +59,19 @@ class Fly extends Cheat{
         }
     }
 
+    public function onHit(EntityDamageByEntityEvent $event) : void{
+        $damaged = $event->getEntity();
+        if($damaged instanceof Player){
+            $this->wasDamaged[$damaged->getName()] = $this->getServer()->getTick();
+        }
+    }
+
     public function onQuit(PlayerQuitEvent $event) : void{
         unset($this->ticksOffGround[$event->getPlayer()->getName()]);
+    }
+
+    private function wasPreviouslyHit(string $name) : bool{
+        return isset($this->wasDamaged[$name]) ? $this->getServer()->getTick() - $this->wasDamaged[$name] <= 10 : false;
     }
 
 }
