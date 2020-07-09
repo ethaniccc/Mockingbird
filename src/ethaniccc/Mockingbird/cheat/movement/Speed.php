de226427a1c8d5987d1145e6efd124e803013a98
ethaniccc <schooldocumentsandstuff@gmail.com>
2020-07-09 07:06:04 -0700

    Test fix for Speed checks

--- src/ethaniccc/Mockingbird/cheat/movement/Speed.php
+++ src/ethaniccc/Mockingbird/cheat/movement/Speed.php
@@ -33,6 +33,10 @@ use pocketmine\block\PackedIce;
 class Speed extends Cheat implements StrictRequirements{
 
     private $suspicionLevel = [];
+    private $ticksOnIce = [];
+    private $wasOnIce = [];
+
+    private $overkillSpeed = [];
 
     public function __construct(Mockingbird $plugin, string $cheatName, string $cheatType, bool $enabled = true){
         parent::__construct($plugin, $cheatName, $cheatType, $enabled);
@@ -58,19 +62,34 @@ class Speed extends Cheat implements StrictRequirements{
         //$this->getServer()->broadcastMessage("BPS: $blocksPerSecond");
         if(!isset($this->suspicionLevel[$name])){
             $this->suspicionLevel[$name] = 0;
-            return;
         }
 
-        if($player->isOnGround()){
-            $expectedMaxSpeed = 5.7;
-        } else {
-            $expectedMaxSpeed = 7.6;
-        }
+        $expectedMaxSpeed = 8;
         if($player->getLevel()->getBlock($player->asVector3()->add(0, 2, 0))->getId() !== 0){
             return;
         }
         if(in_array($player->getLevel()->getBlock($player->asVector3()->subtract(0, 1, 0))->getId(), [BlockIds::ICE, BlockIds::FROSTED_ICE, BlockIds::PACKED_ICE])){
+            if(!isset($this->ticksOnIce[$name])){
+                $this->ticksOnIce[$name] = 0;
+            }
+            $this->ticksOnIce[$name] += 1;
+            $this->wasOnIce[$name] = $this->getServer()->getTick();
             $expectedMaxSpeed *= (5 / 3);
+            $expectedMaxSpeed += $this->ticksOnIce[$name] * 0.02;
+        } else {
+            if(!$player->isOnGround()){
+                if(in_array($player->getLevel()->getBlock($player->asVector3()->subtract(0, 1.5, 0))->getId(), [BlockIds::ICE, BlockIds::FROSTED_ICE, BlockIds::PACKED_ICE])){
+                    if(!isset($this->ticksOnIce[$name])){
+                        $this->ticksOnIce[$name] = 0;
+                    }
+                    $this->ticksOnIce[$name] += 1;
+                    $this->wasOnIce[$name] = $this->getServer()->getTick();
+                    $expectedMaxSpeed *= (5 / 3);
+                    $expectedMaxSpeed += $this->ticksOnIce[$name] * 0.02;
+                }
+            } else {
+                unset($this->ticksOnIce[$name]);
+            }
         }
         if($player->getEffect(1) !== null){
             $level = $player->getEffect(1)->getEffectLevel() + 1;
@@ -78,12 +97,40 @@ class Speed extends Cheat implements StrictRequirements{
             $expectedMaxSpeed *= $multiplier;
         }
 
-        if($blocksPerSecond > $expectedMaxSpeed){
-            if($blocksPerSecond >= $expectedMaxSpeed * 1.45 && $blocksPerSecond <= $expectedMaxSpeed * 2.25){
-                // Spike???
-                $this->suspicionLevel[$name] *= 0.9;
+        if($blocksPerSecond >= $expectedMaxSpeed * 1.45 && $blocksPerSecond <= $expectedMaxSpeed * 2.25){
+            if($player->isSprinting()){
+                if($player->isOnGround()){
+                    if(!in_array($player->getLevel()->getBlock($player->asVector3()->subtract(0, 1, 0))->getId(), [BlockIds::ICE, BlockIds::FROSTED_ICE, BlockIds::PACKED_ICE])){
+                        if($this->wasOnIce($name)){
+                            return;
+                        }
+                    }
+                } else {
+                    if(in_array($player->getLevel()->getBlock($player->asVector3()->subtract(0, 1.5, 0))->getId(), [BlockIds::ICE, BlockIds::FROSTED_ICE, BlockIds::PACKED_ICE])){
+                        if($this->wasOnIce($name)){
+                            return;
+                        }
+                    }
+                }
+                if(!isset($this->overkillSpeed[$name])){
+                    $this->overkillSpeed[$name] = 0;
+                }
+                $this->overkillSpeed[$name] += 1;
+                if($this->overkillSpeed[$name] >= 30){
+                    $this->addViolation($name);
+                    $this->notifyStaff($name, $this->getName(), $this->genericAlertData($player));
+                    $this->overkillSpeed[$name] *= 0.5;
+                }
+                if(isset($this->suspicionLevel[$name])){
+                    $this->suspicionLevel[$name] *= 0.75;
+                }
                 return;
             }
+        } else {
+            unset($this->overkillSpeed[$name]);
+        }
+
+        if($blocksPerSecond > $expectedMaxSpeed){
             $this->suspicionLevel[$name] += 1;
             if($this->suspicionLevel[$name] >= 5){
                 $this->addViolation($name);
@@ -93,6 +140,10 @@ class Speed extends Cheat implements StrictRequirements{
         } else {
             $this->suspicionLevel[$name] *= 0.75;
         }
+    }
+
+    private function wasOnIce(string $name) : bool{
+        return isset($this->wasOnIce[$name]) ? $this->getServer()->getTick() - $this->wasOnIce[$name] <= 40 : false;
     }
 
 }
