d7f1b40119ec239d1d4ee85914ce6421f569388c
ethaniccc <schooldocumentsandstuff@gmail.com>
2020-07-25 09:24:07 -0700

    fix timer?

--- src/ethaniccc/Mockingbird/cheat/other/Timer.php
+++ src/ethaniccc/Mockingbird/cheat/other/Timer.php
@@ -4,73 +4,49 @@ namespace ethaniccc\Mockingbird\cheat\other;
 
 use ethaniccc\Mockingbird\cheat\Cheat;
 use ethaniccc\Mockingbird\cheat\StrictRequirements;
+use ethaniccc\Mockingbird\event\MoveEvent;
 use ethaniccc\Mockingbird\Mockingbird;
-use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\network\mcpe\protocol\MovePlayerPacket;
 
 class Timer extends Cheat implements StrictRequirements{
 
     /** @var array */
-    private $lastSentTime = [];
-
-    /** @var array */
-    private $previousTimeDiff = [];
-
-    /** @var array */
-    private $balance = [];
-
-    /** @var array */
-    private $counter = [];
+    private $balance, $previousTimeDiff, $lastSentTick = [];
 
     public function __construct(Mockingbird $plugin, string $cheatName, string $cheatType, bool $enabled = true){
         parent::__construct($plugin, $cheatName, $cheatType, $enabled);
-        $this->setRequiredPing(10000);
         $this->setRequiredTPS(20.0);
+        $this->setRequiredPing(10000);
     }
 
-    public function receivePacket(DataPacketReceiveEvent $event) : void{
-        $packet = $event->getPacket();
+    public function onMove(MoveEvent $event) : void{
         $player = $event->getPlayer();
         $name = $player->getName();
-
-        if($packet instanceof MovePlayerPacket){
-            $currentTime = $this->getServer()->getTick() * 50;
-            if(!isset($this->lastSentTime[$name])){
-                $this->lastSentTime[$name] = $currentTime;
-                return;
-            }
-            $timeDiff = round($currentTime - $this->lastSentTime[$name], 2);
-            if(!isset($this->balance[$name])){
-                $this->balance[$name] = 0;
-            }
-            $this->balance[$name] = $this->balance[$name] - 50 + $timeDiff;
-            if(isset($this->previousTimeDiff[$name])){
-                if($this->previousTimeDiff[$name] > 100 && $timeDiff <= 100){
-                    $this->balance[$name] = 0;
-                }
-            }
-            if($this->balance[$name] < 0){
-                if(!isset($this->counter[$name])){
-                    $this->counter[$name] = 0;
-                }
-                ++$this->counter[$name];
-                if($this->counter[$name] === 100){
-                    if($this->balance[$name] >= -200){
-                        $this->balance[$name] = 0;
-                    }
-                    $this->counter[$name] = 0;
-                }
-            } else {
-                $this->counter[$name] = 0;
-            }
-            if($this->balance[$name] <= -300){
-                $this->addViolation($name);
-                $this->notifyStaff($name, $this->getName(), $this->genericAlertData($player));
-                $this->balance[$name] = 0;
-            }
-            $this->lastSentTime[$name] = $currentTime;
-            $this->previousTimeDiff[$name] = $timeDiff;
+        if(!isset($this->balance[$name])){
+            $this->balance[$name] = 0;
         }
+        if(!isset($this->lastSentTick[$name])){
+            $this->lastSentTick[$name] = $this->getServer()->getTick();
+            return;
+        }
+
+        $time = ($this->getServer()->getTick() - $this->lastSentTick[$name]) * 50;
+        $this->balance[$name] += 50;
+        $this->balance[$name] -= $time;
+
+        if(isset($this->previousTimeDiff[$name])){
+            if($this->previousTimeDiff[$name] > 100 && ($time <= 100 && $time >= 50)){
+                $this->balance[$name] = 0;
+            }
+        }
+
+        if($this->balance[$name] >= 500){
+            $this->addViolation($name);
+            $this->notify("$name failed a check for Timer");
+            $this->balance[$name] = 0;
+        }
+
+        $this->lastSentTick[$name] = $this->getServer()->getTick();
+        $this->previousTimeDiff[$name] = $time;
     }
 
 }
