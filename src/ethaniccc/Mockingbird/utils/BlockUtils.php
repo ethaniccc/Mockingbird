e005f619c8efcd96c44b12c4a4064ad5f0c9cf60
Misaka_Misaki <61568071+Blackjack200@users.noreply.github.com>
2020-08-25 21:53:12 +0800

    ??????

--- src/ethaniccc/Mockingbird/utils/BlockUtils.php
+++ src/ethaniccc/Mockingbird/utils/BlockUtils.php
@@ -18,13 +18,14 @@ Github: https://github.com/ethaniccc/Mockingbird
 namespace ethaniccc\Mockingbird\utils;
 
 
+use pocketmine\block\Air;
 use pocketmine\block\Block;
 use pocketmine\block\Fallable;
 use pocketmine\block\Transparent;
 
 class BlockUtils {
 	public static function canClip(Block $block) : bool {
-		if($block instanceof Fallable || $block instanceof Transparent) {
+		if($block instanceof Fallable || $block instanceof Transparent || $block instanceof Air) {
 			return true;
 		}
 		return false;
