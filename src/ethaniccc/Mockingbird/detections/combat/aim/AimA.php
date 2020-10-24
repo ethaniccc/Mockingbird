ce6dae59a5fc5f5c4295b41f9d9d071ae9632d41
ethaniccc <schooldocumentsandstuff@gmail.com>
2020-10-24 14:15:54 -0700

    add AimA

---
+++ src/ethaniccc/Mockingbird/detections/combat/aim/AimA.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ethaniccc\Mockingbird\detections\combat\aim;
+
+use ethaniccc\Mockingbird\detections\Detection;
+use ethaniccc\Mockingbird\user\User;
+use pocketmine\network\mcpe\protocol\DataPacket;
+use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
+
+class AimA extends Detection{
+
+    public function __construct(string $name, ?array $settings){
+        parent::__construct($name, $settings);
+    }
+
+    public function handle(DataPacket $packet, User $user): void{
+        if($packet instanceof PlayerAuthInputPacket){
+            $yawDelta = $user->yawDelta;
+            $pitchDelta = $user->pitchDelta;
+            if($user->timeSinceAttack <= 10){
+                $this->debug($pitchDelta, false);
+                // impossible for pitch delta to be so low in this case
+                if($yawDelta > 3 && $pitchDelta <= 2E-5 && abs($user->pitch) < 90){
+                    if(++$this->preVL >= 3){
+                        $this->fail($user, "yawDelta=$yawDelta pitchDelta=$pitchDelta");
+                    }
+                } else {
+                    $this->preVL -= $this->preVL > 0 ? 1 : 0;
+                    $this->reward($user, 0.999);
+                }
+            }
+        }
+    }
+
+}
