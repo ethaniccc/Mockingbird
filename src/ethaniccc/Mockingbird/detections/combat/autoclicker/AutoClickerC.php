7889dfcda9a7fa6f63aea260941a5045f476f01d
ethaniccc <schooldocumentsandstuff@gmail.com>
2020-11-01 16:57:05 -0500

    Add config options for AutoClickerC

--- src/ethaniccc/Mockingbird/detections/combat/autoclicker/AutoClickerC.php
+++ src/ethaniccc/Mockingbird/detections/combat/autoclicker/AutoClickerC.php
@@ -26,10 +26,10 @@ class AutoClickerC extends Detection{
         if(($packet instanceof InventoryTransactionPacket && $packet->transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY) || ($packet instanceof LevelSoundEventPacket && $packet->sound === LevelSoundEventPacket::SOUND_ATTACK_NODAMAGE)){
             if($this->movements <= 4){
                 $this->delaySamples[] = $this->movements;
-                if(count($this->delaySamples) === 30){
+                if(count($this->delaySamples) === $this->getSetting("samples")){
                     $skewness = MathUtils::getSkewness($this->delaySamples);
                     $kurtosis = MathUtils::getKurtosis($this->delaySamples);
-                    if($skewness <= 0 && $kurtosis < 0.02){
+                    if($skewness <= $this->getSetting("skewness") && $kurtosis < $this->getSetting("kurtosis")){
                         $this->fail($user, "skewness=$skewness kurtosis=$kurtosis probability={$this->getCheatProbability()}");
                     }
                     $this->delaySamples = [];
