89e5c398976b2ac0547236f30daa8ebb3e7e6caf
ethaniccc <schooldocumentsandstuff@gmail.com>
2020-11-16 07:51:45 -0500

    update config feature (removes config comments :C)

--- src/ethaniccc/Mockingbird/Mockingbird.php
+++ src/ethaniccc/Mockingbird/Mockingbird.php
@@ -13,6 +13,7 @@ use ethaniccc\Mockingbird\tasks\DebugLogWriteTask;
 use ethaniccc\Mockingbird\user\UserManager;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
 class Mockingbird extends PluginBase{
@@ -31,14 +32,22 @@ class Mockingbird extends PluginBase{
         if(self::$instance !== null){
             return;
         }
+        $this->debugTask = new DebugLogWriteTask($this->getDataFolder() . "debug_log.txt");
         file_put_contents($this->getDataFolder() . "debug_log.txt", "");
         self::$instance = $this;
+        if($this->getDescription()->getVersion() !== $this->getConfig()->get("version")){
+            if($this->updateConfig()){
+                $this->getLogger()->debug("Mockingbird config has been updated");
+                $this->getConfig()->reload();
+            } else {
+                $this->getLogger()->alert("Something went wrong while updating the config, please go manually edit the new config.");
+            }
+        }
         UserManager::init();
         new MockingbirdListener();
         $this->getAvailableProcessors();
         $this->getAvailableChecks();
         $this->registerCommands();
-        $this->debugTask = new DebugLogWriteTask($this->getDataFolder() . "debug_log.txt");
         $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(int $currentTick) : void{
             $this->getServer()->getAsyncPool()->submitTask($this->debugTask);
             $this->debugTask = new DebugLogWriteTask($this->getDataFolder() . "debug_log.txt");
@@ -135,6 +144,30 @@ class Mockingbird extends PluginBase{
                 } catch(\ReflectionException $e){}
             }
         }
+    }
+
+    private function updateConfig() : bool{
+        // get all the previous settings the user has
+        $oldConfig = $this->getConfig()->getAll();
+        // remove the version - it's (most likely) outdated
+        unset($oldConfig["version"]);
+        @unlink($this->getConfig()->getPath());
+        $this->reloadConfig();
+        foreach($oldConfig as $key => $value){
+            // if the setting found in the old config is not in the current config,
+            // the old config is (probably) too old.
+            if(!isset($this->getConfig()->getAll()[$key])){
+                @unlink($this->getConfig()->getPath());
+                $this->reloadConfig();
+                return false;
+            }
+            // set the current config setting value to the old config setting value
+            $this->getConfig()->set($key, $value);
+        }
+        // save the config - (why are comments being deleted here?)
+        // TODO: Make some sort of hack to bring back the config notes :C
+        $this->getConfig()->save();
+        return true;
     }
 
     public function onDisable(){
