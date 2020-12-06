cb5425b295e53376d43c5d09146b4079f31aaf4b
ethaniccc <schooldocumentsandstuff@gmail.com>
2020-12-06 16:10:15 -0500

    attempt 2 at fixing this mess

--- src/ethaniccc/Mockingbird/detections/combat/reach/ReachA.php
+++ src/ethaniccc/Mockingbird/detections/combat/reach/ReachA.php
@@ -1,1 +1,58 @@
-<?phpnamespace ethaniccc\Mockingbird\detections\combat\reach;use ethaniccc\Mockingbird\detections\Detection;use ethaniccc\Mockingbird\user\User;use ethaniccc\Mockingbird\utils\boundingbox\AABB;use ethaniccc\Mockingbird\utils\SizedList;use pocketmine\network\mcpe\protocol\DataPacket;use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;class ReachA extends Detection{    private $appendingMove = false;    public function __construct(string $name, ?array $settings){        parent::__construct($name, $settings);        $this->vlThreshold = 20;    }    public function handle(DataPacket $packet, User $user): void{        if($packet instanceof InventoryTransactionPacket && $user->win10 && !$user->player->isCreative() && !$this->appendingMove && $packet->transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY && $packet->trData->actionType === InventoryTransactionPacket::USE_ITEM_ON_ENTITY_ACTION_ATTACK){            if($user->tickData->targetLocationHistory->getLocations()->full()){                // wait for the next PlayerAuthInputPacket from the client                $this->appendingMove = true;            }        } elseif($packet instanceof PlayerAuthInputPacket && $this->appendingMove){            $locations = $user->tickData->targetLocationHistory->getLocationsRelativeToTime($user->tickData->currentTick - floor($user->transactionLatency / 50), 2);            // 40 is the max location history size in the tick data            $distances = new SizedList(40);            foreach($locations as $location){                $AABB = AABB::fromPosition($location)->expand(0.1, 0.1, 0.1);                // more lightweight way for calculating reach                $distances->add($AABB->distanceFromVector($packet->getPosition()));            }            $distance = $distances->minOrElse(-1);            if($distance !== -1){                if($distance > $this->getSetting("max_reach")){                    $this->preVL += 1.5;                    // sometimes the preVL can reach to 4, I put 4.1 here as I haven't seen the preVL (on localhost testing) go above 4                    if($this->preVL >= 4.1){                        $this->preVL = min($this->preVL, 9);                        $this->fail($user, "dist=$distance");                    }                } else {                    $this->reward($user, 0.9995);                    $this->preVL = max($this->preVL - 0.75, 0);                }            }            if($this->isDebug($user)){                $user->sendMessage("dist=$distance buff={$this->preVL}");            }            $this->appendingMove = false;        }    }}
+<?php
+
+namespace ethaniccc\Mockingbird\detections\combat\reach;
+
+use ethaniccc\Mockingbird\detections\Detection;
+use ethaniccc\Mockingbird\user\User;
+use ethaniccc\Mockingbird\utils\boundingbox\AABB;
+use ethaniccc\Mockingbird\utils\SizedList;
+use pocketmine\network\mcpe\protocol\DataPacket;
+use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
+use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
+
+class ReachA extends Detection{
+
+    private $appendingMove = false;
+
+    public function __construct(string $name, ?array $settings){
+        parent::__construct($name, $settings);
+        $this->vlThreshold = 20;
+    }
+
+    public function handle(DataPacket $packet, User $user) : void{
+        if($packet instanceof InventoryTransactionPacket && $user->win10 && !$user->player->isCreative() && !$this->appendingMove && $packet->transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY && $packet->trData->actionType === InventoryTransactionPacket::USE_ITEM_ON_ENTITY_ACTION_ATTACK){
+            if($user->tickData->targetLocationHistory->getLocations()->full()){
+                // wait for the next PlayerAuthInputPacket from the client
+                $this->appendingMove = true;
+            }
+        } elseif($packet instanceof PlayerAuthInputPacket && $this->appendingMove){
+            $locations = $user->tickData->targetLocationHistory->getLocationsRelativeToTime($user->tickData->currentTick - floor($user->transactionLatency / 50), 2);
+            // 40 is the max location history size in the tick data
+            $distances = new SizedList(40);
+            foreach($locations as $location){
+                $AABB = AABB::fromPosition($location)->expand(0.1, 0.1, 0.1);
+                // more lightweight way for calculating reach
+                $distances->add($AABB->distanceFromVector($packet->getPosition()));
+            }
+            $distance = $distances->minOrElse(-1);
+            if($distance !== -1){
+                if($distance > $this->getSetting("max_reach")){
+                    $this->preVL += 1.5;
+                    // sometimes the preVL can reach to 4, I put 4.1 here as I haven't seen the preVL (on localhost testing) go above 4
+                    if($this->preVL >= 4.1){
+                        $this->preVL = min($this->preVL, 9);
+                        $this->fail($user, "dist=$distance");
+                    }
+                } else {
+                    $this->reward($user, 0.9995);
+                    $this->preVL = max($this->preVL - 0.75, 0);
+                }
+            }
+            if($this->isDebug($user)){
+                $user->sendMessage("dist=$distance buff={$this->preVL}");
+            }
+            $this->appendingMove = false;
+        }
+    }
+
+}
--- src/ethaniccc/Mockingbird/utils/boundingbox/AABB.php
+++ src/ethaniccc/Mockingbird/utils/boundingbox/AABB.php
@@ -1,1 +1,146 @@
-<?phpnamespace ethaniccc\Mockingbird\utils\boundingbox;use ethaniccc\Mockingbird\user\User;use pocketmine\block\Block;use pocketmine\math\AxisAlignedBB;use pocketmine\math\Vector3;class AABB extends AxisAlignedBB{    public $minX, $minY, $minZ;    public $maxX, $maxY, $maxZ;    public $minVector, $maxVector;    public function __construct(float $minX, float $minY, float $minZ, float $maxX, float $maxY, float $maxZ) {        parent::__construct($minX, $minY, $minZ, $maxX, $maxX, $maxZ);        $this->minX = $minX;        $this->minY = $minY;        $this->minZ = $minZ;        $this->maxX = $maxX;        $this->maxY = $maxY;        $this->maxZ = $maxZ;        $this->minVector = new Vector3($this->minX, $this->minY, $this->minZ);        $this->maxVector = new Vector3($this->maxX, $this->maxY, $this->maxZ);    }    public static function from(User $user) : AABB{        $pos = $user->moveData->location;        return new AABB($pos->x - 0.3, $pos->y, $pos->z - 0.3, $pos->x + 0.3, $pos->y + 1.8, $pos->z + 0.3);    }    public static function fromPosition(Vector3 $pos) : AABB{        return new AABB($pos->x - 0.3, $pos->y, $pos->z - 0.3, $pos->x + 0.3, $pos->y + 1.8, $pos->z + 0.3);    }    public static function fromBlock(Block $block) : AABB{        $b = $block->getBoundingBox();        if($b !== null) {            return new AABB(                $b->minX, $b->minY, $b->minZ,                $b->maxX, $b->maxY, $b->maxZ            );        } else {            // apparently some blocks (Cobweb in my case) have no AABB            return new AABB(                $block->getX(), $block->getY(), $block->getZ(),                $block->getX() + 1, $block->getY() + 1, $block->getZ() + 1            );        }    }    public function translate(float $x, float $y, float $z) : AABB{        return new AABB($this->minX + $x, $this->minY + $y, $this->minZ + $z, $this->maxX + $x, $this->maxY, $this->maxZ);    }    public function grow(float $x, float $y, float $z) : AABB{        return new AABB($this->minX - $x, $this->minY - $y, $this->minZ - $z, $this->maxX + $x, $this->maxY, $this->maxZ);    }    public function stretch(float $x, float $y, float $z) : AABB{        return new AABB($this->minX, $this->minY, $this->minZ, $this->maxX + $x, $this->maxY, $this->maxZ);    }    public function contains(Vector3 $pos) : bool{        return $pos->getX() <= $this->maxX            && $pos->getY() <= $this->maxY            && $pos->getZ() <= $this->maxZ            && $pos->getX() >= $this->minX            && $pos->getY() >= $this->minY            && $pos->getZ() >= $this->minZ;    }    public function min(int $i) : float{        return [$this->minX, $this->minY, $this->minZ][$i] ?? 0;    }    public function max(int $i) : float{        return [$this->maxX, $this->maxY, $this->maxZ][$i] ?? 0;    }    public function getCornerVectors() : array{        return [            // top vectors            new Vector3($this->maxX, $this->maxY, $this->maxZ),            new Vector3($this->minX, $this->maxY, $this->maxZ),            new Vector3($this->minX, $this->maxY, $this->minZ),            new Vector3($this->maxX, $this->maxY, $this->minZ),            // bottom vectors            new Vector3($this->maxX, $this->minY, $this->maxZ),            new Vector3($this->minX, $this->minY, $this->maxZ),            new Vector3($this->minX, $this->minY, $this->minZ),            new Vector3($this->maxX, $this->minY, $this->minZ)        ];    }    public function distanceFromVector(Vector3 $vector) : float{        $distX = max($this->minX - $vector->x, max(0, $vector->x - $this->maxX));        $distY = max($this->minY - $vector->y, max(0, $vector->y - $this->maxY));        $distZ = max($this->minZ - $vector->z, max(0, $vector->z - $this->maxZ));        return sqrt(($distX ** 2) + ($distY ** 2) + ($distZ ** 2));    }    public function collidesRay(Ray $ray, float $maxDist) : float{        $pos1 = $ray->getOrigin();        if($this->isVectorInside($pos1)){            return 0.0;        }        $pos2 = $pos1->add($ray->getDirection()->multiply($maxDist));        $v1 = $pos1->getIntermediateWithXValue($pos2, $this->minX);        $v2 = $pos1->getIntermediateWithXValue($pos2, $this->maxX);        $v3 = $pos1->getIntermediateWithYValue($pos2, $this->minY);        $v4 = $pos1->getIntermediateWithYValue($pos2, $this->maxY);        $v5 = $pos1->getIntermediateWithZValue($pos2, $this->minZ);        $v6 = $pos1->getIntermediateWithZValue($pos2, $this->maxZ);        if($v1 !== null and !$this->isVectorInYZ($v1)){            $v1 = null;        }        if($v2 !== null and !$this->isVectorInYZ($v2)){            $v2 = null;        }        if($v3 !== null and !$this->isVectorInXZ($v3)){            $v3 = null;        }        if($v4 !== null and !$this->isVectorInXZ($v4)){            $v4 = null;        }        if($v5 !== null and !$this->isVectorInXY($v5)){            $v5 = null;        }        if($v6 !== null and !$this->isVectorInXY($v6)){            $v6 = null;        }        $vector = null;        $distance = PHP_INT_MAX;        foreach([$v1, $v2, $v3, $v4, $v5, $v6] as $v){            if($v !== null and ($d = $pos1->distanceSquared($v)) < $distance){                $vector = $v;                $distance = $d;            }        }        return $vector !== null ? sqrt($distance) : -69.0;    }}
+<?php
+
+namespace ethaniccc\Mockingbird\utils\boundingbox;
+
+use ethaniccc\Mockingbird\user\User;
+use pocketmine\block\Block;
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\math\Vector3;
+
+class AABB extends AxisAlignedBB{
+
+    public $minX, $minY, $minZ;
+    public $maxX, $maxY, $maxZ;
+    public $minVector, $maxVector;
+
+    public function __construct(float $minX, float $minY, float $minZ, float $maxX, float $maxY, float $maxZ) {
+        parent::__construct($minX, $minY, $minZ, $maxX, $maxX, $maxZ);
+        $this->minX = $minX;
+        $this->minY = $minY;
+        $this->minZ = $minZ;
+        $this->maxX = $maxX;
+        $this->maxY = $maxY;
+        $this->maxZ = $maxZ;
+        $this->minVector = new Vector3($this->minX, $this->minY, $this->minZ);
+        $this->maxVector = new Vector3($this->maxX, $this->maxY, $this->maxZ);
+    }
+
+    public static function from(User $user) : AABB{
+        $pos = $user->moveData->location;
+        return new AABB($pos->x - 0.3, $pos->y, $pos->z - 0.3, $pos->x + 0.3, $pos->y + 1.8, $pos->z + 0.3);
+    }
+
+    public static function fromPosition(Vector3 $pos) : AABB{
+        return new AABB($pos->x - 0.3, $pos->y, $pos->z - 0.3, $pos->x + 0.3, $pos->y + 1.8, $pos->z + 0.3);
+    }
+
+    public static function fromBlock(Block $block) : AABB{
+        $b = $block->getBoundingBox();
+        if($b !== null) {
+            return new AABB(
+                $b->minX, $b->minY, $b->minZ,
+                $b->maxX, $b->maxY, $b->maxZ
+            );
+        } else {
+            // apparently some blocks (Cobweb in my case) have no AABB
+            return new AABB(
+                $block->getX(), $block->getY(), $block->getZ(),
+                $block->getX() + 1, $block->getY() + 1, $block->getZ() + 1
+            );
+        }
+    }
+
+    public function translate(float $x, float $y, float $z) : AABB{
+        return new AABB($this->minX + $x, $this->minY + $y, $this->minZ + $z, $this->maxX + $x, $this->maxY, $this->maxZ);
+    }
+
+    public function grow(float $x, float $y, float $z) : AABB{
+        return new AABB($this->minX - $x, $this->minY - $y, $this->minZ - $z, $this->maxX + $x, $this->maxY, $this->maxZ);
+    }
+
+    public function stretch(float $x, float $y, float $z) : AABB{
+        return new AABB($this->minX, $this->minY, $this->minZ, $this->maxX + $x, $this->maxY, $this->maxZ);
+    }
+
+    public function contains(Vector3 $pos) : bool{
+        return $pos->getX() <= $this->maxX
+            && $pos->getY() <= $this->maxY
+            && $pos->getZ() <= $this->maxZ
+            && $pos->getX() >= $this->minX
+            && $pos->getY() >= $this->minY
+            && $pos->getZ() >= $this->minZ;
+    }
+
+    public function min(int $i) : float{
+        return [$this->minX, $this->minY, $this->minZ][$i] ?? 0;
+    }
+
+    public function max(int $i) : float{
+        return [$this->maxX, $this->maxY, $this->maxZ][$i] ?? 0;
+    }
+
+    public function getCornerVectors() : array{
+        return [
+            // top vectors
+            new Vector3($this->maxX, $this->maxY, $this->maxZ),
+            new Vector3($this->minX, $this->maxY, $this->maxZ),
+            new Vector3($this->minX, $this->maxY, $this->minZ),
+            new Vector3($this->maxX, $this->maxY, $this->minZ),
+            // bottom vectors
+            new Vector3($this->maxX, $this->minY, $this->maxZ),
+            new Vector3($this->minX, $this->minY, $this->maxZ),
+            new Vector3($this->minX, $this->minY, $this->minZ),
+            new Vector3($this->maxX, $this->minY, $this->minZ)
+        ];
+    }
+
+    public function distanceFromVector(Vector3 $vector) : float{
+        $distX = max($this->minX - $vector->x, max(0, $vector->x - $this->maxX));
+        $distY = max($this->minY - $vector->y, max(0, $vector->y - $this->maxY));
+        $distZ = max($this->minZ - $vector->z, max(0, $vector->z - $this->maxZ));
+        return sqrt(($distX ** 2) + ($distY ** 2) + ($distZ ** 2));
+    }
+
+    public function collidesRay(Ray $ray, float $maxDist) : float{
+        $pos1 = $ray->getOrigin();
+        if($this->isVectorInside($pos1)){
+            return 0.0;
+        }
+        $pos2 = $pos1->add($ray->getDirection()->multiply($maxDist));
+        $v1 = $pos1->getIntermediateWithXValue($pos2, $this->minX);
+        $v2 = $pos1->getIntermediateWithXValue($pos2, $this->maxX);
+        $v3 = $pos1->getIntermediateWithYValue($pos2, $this->minY);
+        $v4 = $pos1->getIntermediateWithYValue($pos2, $this->maxY);
+        $v5 = $pos1->getIntermediateWithZValue($pos2, $this->minZ);
+        $v6 = $pos1->getIntermediateWithZValue($pos2, $this->maxZ);
+        if($v1 !== null and !$this->isVectorInYZ($v1)){
+            $v1 = null;
+        }
+        if($v2 !== null and !$this->isVectorInYZ($v2)){
+            $v2 = null;
+        }
+        if($v3 !== null and !$this->isVectorInXZ($v3)){
+            $v3 = null;
+        }
+        if($v4 !== null and !$this->isVectorInXZ($v4)){
+            $v4 = null;
+        }
+        if($v5 !== null and !$this->isVectorInXY($v5)){
+            $v5 = null;
+        }
+        if($v6 !== null and !$this->isVectorInXY($v6)){
+            $v6 = null;
+        }
+        $vector = null;
+        $distance = PHP_INT_MAX;
+        foreach([$v1, $v2, $v3, $v4, $v5, $v6] as $v){
+            if($v !== null and ($d = $pos1->distanceSquared($v)) < $distance){
+                $vector = $v;
+                $distance = $d;
+            }
+        }
+
+        return $vector !== null ? sqrt($distance) : -69.0;
+    }
+
+}
